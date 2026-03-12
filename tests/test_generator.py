b666e640c3d696d5192e5f5d6163fe91030c4b04
Leonel <mendiolaleonel2002@gmail.com>
2026-03-11 20:51:59 -0600

    Update test_generator file

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,6 +1,6 @@
 from generator.password_generator import (
     generate_password,
-    has_upercase,
+    has_uppercase,
     has_number,
     has_symbol
 )
@@ -12,3 +12,17 @@ def test_password_length():
 def test_password_is_string():
     password = generate_password(10)
     assert isinstance(password, str)
+
+def test_has_uppercase():
+    assert has_uppercase("Abc") is True
+
+def test_has_number():
+    assert has_number("abc1") is True
+
+def test_has_symbol():
+    assert has_symbol("abc!") is True
+
+def test_generated_password_length_range():
+    for length in range (4,20):
+        password = generate_password(length)
+        assert len(password) == length
